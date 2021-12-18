7022d896755ff482b5e3e74c03b68d4ae8585fdf
matiasua <matiasncp7@gmail.com>
2021-12-18 18:34:16 -0300

    add semantic ui react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Button, Icon} from 'semantic-ui-react'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hola!</h1>
+      <div>
+        <Button color='facebook'>
+          <Icon name='facebook' /> Facebook
+        </Button>
+      </div>
+)
     </div>
-  );
+  )
 }
-
-export default App;
