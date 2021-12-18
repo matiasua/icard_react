0fc35ee4480a036c7dc36663b445d824587a1c60
matiasua <matiasncp7@gmail.com>
2021-12-18 20:21:43 -0300

    add sass

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from 'react'
-import {Button, Icon} from 'semantic-ui-react'
 
 export default function App() {
   return (
     <div>
       <h1>Hola!</h1>
-      <div>
-        <Button color='facebook'>
-          <Icon name='facebook' /> Facebook
-        </Button>
-      </div>
-)
     </div>
   )
 }
