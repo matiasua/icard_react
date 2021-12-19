5db16151dfafefe9c26b270c3859e36129021c47
matiasua <matiasncp7@gmail.com>
2021-12-18 21:43:57 -0300

    sistema de rutas OK

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import {Navigation} from './routes'
 
 export default function App() {
   return (
     <div>
       <h1>Hola!</h1>
+      <Navigation />
     </div>
   )
 }
---
+++ src/routes/Navigation.js
@@ -0,0 +1,24 @@
+import { map } from 'lodash';
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import routes from './routes';
+
+export function Navigation() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {map(routes, (route, index) => (
+          <Route
+            key={index}
+            path={route.path}
+            element={
+              <route.layout>
+                <route.component />
+              </route.layout>
+            }
+            />
+        ))}
+      </Routes>
+    </BrowserRouter>
+  )
+}
