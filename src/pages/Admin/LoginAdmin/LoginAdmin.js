c6ab76fdbb55ff648e6785d4420a12ded50eb229
matiasua <matiasncp7@gmail.com>
2021-12-21 19:00:25 -0300

    Estructura del login Admin y Formulario de login

--- src/pages/Admin/LoginAdmin/LoginAdmin.js
+++ src/pages/Admin/LoginAdmin/LoginAdmin.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import {LoginForm} from '../../../components/Admin'
 import './LoginAdmin.scss'
 
 export function LoginAdmin() {
   return (
     <div class='login-admin-container'>
       <div className="login-admin__content">
-        <h1>Entrar al Panel</h1>
-        <p>Formulario</p>
+        <h1>Ingresar al Panel</h1>
+        <LoginForm />
       </div>
     </div>
   )
