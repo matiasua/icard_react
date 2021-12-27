e54aae0a5556b726d6d4506cc28fbf30d2ee0015
matiasua <matiasncp7@gmail.com>
2021-12-27 17:00:15 -0300

    Estado y validacion de form login with formik, yup

--- src/components/Admin/LoginForm/LoginForm.js
+++ src/components/Admin/LoginForm/LoginForm.js
@@ -1,20 +1,57 @@
 import React from 'react'
 import {Button, Form} from 'semantic-ui-react'
+import {useFormik} from 'formik'
+import * as Yup from 'yup'
 import './LoginForm.scss'
 
 export function LoginForm() {
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    onSubmit: (formValue) => {
+      console.log('LOGIN OK')
+      console.log(formValue)
+    },
+  });
+
   return (
-    <Form className='login-form-admin'>
+    <Form className='login-form-admin' onSubmit={formik.handleSubmit}>
       <Form.Input
         name='email'
-        placeholder = 'Correo Electronico'/>
+        placeholder = 'Correo Electronico'
+        value = {formik.values.email}
+        onChange={formik.handleChange}
+        error={formik.errors.email}
+        />
       <Form.Input
         name='password'
         type='password'
-        placeholder = 'Contraseña'/>
+        placeholder = 'Contraseña'
+        value = {formik.values.password}
+        onChange={formik.handleChange}
+        error={formik.errors.password}
+        />
         <Button
           type='submit'
           content='Iniciar Sesión' primary fluid/>
     </Form>
   )
+}
+
+function initialValues() {
+  return {
+    email: '',
+    password: ''
+  }
+}
+
+function validationSchema() {
+  return {
+    email: Yup.string()
+      .email('Correo Electronico Invalido')
+      .required('Correo Electronico Requerido'),
+    password: Yup.string()
+      .min(6, 'Contraseña debe tener al menos 6 caracteres')
+      .required('Contraseña Requerida')
+  }
 }
