870510aba4d8f20b70b240f92a66fadbbb03e245
matiasua <matiasncp7@gmail.com>
2021-12-31 21:30:45 -0300

    TopMenu

--- src/components/Admin/TopMenu/TopMenu.js
+++ src/components/Admin/TopMenu/TopMenu.js
@@ -18,7 +18,7 @@ export  function TopMenu() {
   return (
     <Menu fixed='top' className='top-menu-admin'>
       <Menu.Item className='top-menu-admin__logo'>
-        <p>ICARD ADMIN</p>
+        <p>iCard Admin</p>
       </Menu.Item>
 
       <Menu.Menu position='right'>
