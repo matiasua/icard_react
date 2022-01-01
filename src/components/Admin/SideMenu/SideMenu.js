effbe076e0ad279552910cb0a96a683ac6701329
matiasua <matiasncp7@gmail.com>
2021-12-31 21:17:53 -0300

    Manejo de restriccion de Usuarios para user Admin

--- src/components/Admin/SideMenu/SideMenu.js
+++ src/components/Admin/SideMenu/SideMenu.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Menu, Icon} from 'semantic-ui-react'
 import {Link, useLocation} from 'react-router-dom'
+import {useAuth} from '../../../hooks'
 import './SideMenu.scss'
 
 export function SideMenu(props) {
@@ -18,6 +19,8 @@ export function SideMenu(props) {
 
 function MenuLeft(props) {
   const {pathname} = props;
+
+  const {auth} = useAuth();
 
   return(
     <Menu fixed='left' borderless className='side' vertical>
@@ -42,9 +45,11 @@ function MenuLeft(props) {
         <Icon name='food' /> Productos
         </Menu.Item>
 
-        <Menu.Item as={Link} to={'/admin/users'} active={pathname === '/admin/users'}>
-        <Icon name='users' /> Usuarios
-        </Menu.Item>
+        {auth.user?.is_staff && (
+          <Menu.Item as={Link} to={'/admin/users'} active={pathname === '/admin/users'}>
+          <Icon name='users' /> Usuarios
+          </Menu.Item>
+        )}
     </Menu>
   )
 }
