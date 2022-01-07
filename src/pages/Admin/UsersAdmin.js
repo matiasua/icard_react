0d34462d11ca69d901ee42b5dc7477451b31d385
matiasua <matiasncp7@gmail.com>
2022-01-07 18:04:50 -0300

    getting all users for administrators

--- src/pages/Admin/UsersAdmin.js
+++ src/pages/Admin/UsersAdmin.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import {useUser} from '../../hooks'
 
 export function UsersAdmin() {
+  const {loading, users, getUsers} = useUser();
+
+  useEffect(() => getUsers(), []);
+
   return (
     <div>
       <h1>Estamos en Users Admin</h1>
