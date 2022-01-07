2acf4469b20e741d4dd56d85d5a732717ddf2e98
matiasua <matiasncp7@gmail.com>
2022-01-07 20:45:10 -0300

    Created Modal Basic

--- src/pages/Admin/UsersAdmin.js
+++ src/pages/Admin/UsersAdmin.js
@@ -1,16 +1,22 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Loader } from 'semantic-ui-react';
 import { HeaderPage, TableUsers }  from '../../components/Admin'
+import {  ModalBasic } from '../../components/Common'
 import { useUser } from '../../hooks'
 
 export function UsersAdmin() {
+  const [showModal, setShowModal] = useState(false)
+  const [titleModal, setTitleModal] = useState(null)
+  const [ContentModal, setContentModal] = useState(null)
   const {loading, users, getUsers} = useUser();
 
   useEffect(() => getUsers(), []);
 
+  const openCloseModal = () => setShowModal((prev) => !prev);
+
   return (
     <div>
-      <HeaderPage title = 'Usuarios' btnTitle='Nuevo usuario'/>
+      <HeaderPage title = 'Usuarios' btnTitle='Nuevo usuario' btnClick={openCloseModal}/>
       {loading ? (
         <Loader active inline='centered'>
           Cargando...
@@ -18,6 +24,8 @@ export function UsersAdmin() {
       ) : (
         <TableUsers users={users} />
       )}
+
+      <ModalBasic show={showModal} onClose={openCloseModal} title='Crear usuario' children={<h2>Content...</h2>}/>
     </div>
   )
 }
